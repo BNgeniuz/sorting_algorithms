746125b9b5f5f900393dc20b50b01bdc9b4f1c07
Godwin Bruce <godwin.bruce@gmail.com>
2024-01-23 15:00:04 -0800

    function that sorts an array of integers in ascending order using the Quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+/**
+*swap - swaps the positions of 2 int
+*@x: array element
+*@y: array element
+*/
+void swap(int *x, int *y)
+{
+        int pmt;
+
+        pmt = *x;
+        *x = *y;
+        *y = pmt;
+}
+
+/**
+ * lomuto_qsort - lomuto sorting schemey
+ * @array: Array to input
+ * @first: first value of the array
+ * @last: last value of the array
+ * @size: array size
+ * Return: 0 (void)
+ */
+void lomuto_qsort(int *array, int first, int last, size_t size)
+{
+        int m;
+
+        if (first < last)
+        {
+	       m = partition(array, first, last, size);
+	       lomuto_qsort(array, first, m - 1, size);
+	       lomuto_qsort(array, m + 1, last, size);
+        }
+}
+
+/**
+ * quick_sort - lomuto partition sorting
+ * @array: sorting array to input
+ * @size: size of an array
+ * Return: 0 (void)
+ */
+void quick_sort(int *array, size_t size)
+{
+	lomuto_qsort(array, 0, size - 1, size);
+}
+
+/**
+ * partition - pivotal array
+ * @array: array to input
+ * @first: first integer
+ * @last: last integer
+ * @size: size of array
+ * Return: interger for pivot
+ */
+int partition(int *array, int first, int last, size_t size)
+{
+	int index = array[last];
+	int k = first - 1, m;
+
+	for (m = first; m <= last; m++)
+	{
+		if (array[m] <= index)
+		{
+			k++;
+			if (k != m)
+			{
+				swap(&array[k], &array[m]);
+				print_array(array, size);
+			}
+		}
+	}
+	return (k);
+}
