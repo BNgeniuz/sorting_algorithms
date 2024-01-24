cb250f92fc05276b36b1a76758dabf1b4feb5278
Godwin Bruce <godwin.bruce@gmail.com>
2024-01-23 17:49:44 -0800

    function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,45 +4,15 @@
 *swap - swaps the positions of 2 int
 *@x: array element
 *@y: array element
+Return: 0 (success)
 */
 void swap(int *x, int *y)
 {
-        int pmt;
+	int pmt;
 
-        pmt = *x;
-        *x = *y;
-        *y = pmt;
-}
-
-/**
- * lomuto_qsort - lomuto sorting schemey
- * @array: Array to input
- * @first: first value of the array
- * @last: last value of the array
- * @size: array size
- * Return: 0 (void)
- */
-void lomuto_qsort(int *array, int first, int last, size_t size)
-{
-        int m;
-
-        if (first < last)
-        {
-	       m = partition(array, first, last, size);
-	       lomuto_qsort(array, first, m - 1, size);
-	       lomuto_qsort(array, m + 1, last, size);
-        }
-}
-
-/**
- * quick_sort - lomuto partition sorting
- * @array: sorting array to input
- * @size: size of an array
- * Return: 0 (void)
- */
-void quick_sort(int *array, size_t size)
-{
-	lomuto_qsort(array, 0, size - 1, size);
+	pmt = *x;
+	*x = *y;
+	*y = pmt;
 }
 
 /**
@@ -71,4 +41,35 @@ int partition(int *array, int first, int last, size_t size)
 		}
 	}
 	return (k);
+}
+
+/**
+ * lomuto_qsort - lomuto sorting schemey
+ * @array: Array to input
+ * @first: first value of the array
+ * @last: last value of the array
+ * @size: array size
+ * Return: 0 (void)
+ */
+void lomuto_qsort(int *array, int first, int last, size_t size)
+{
+        int m;
+
+        if (first < last)
+        {
+                m = partition(array, first, last, size);
+                lomuto_qsort(array, first, m - 1, size);
+                lomuto_qsort(array, m + 1, last, size);
+        }
+}
+
+/**
+ * quick_sort - lomuto partition sorting
+ * @array: sorting array to input
+ * @size: size of an array
+ * Return: 0 (void)
+ */
+void quick_sort(int *array, size_t size)
+{
+        lomuto_qsort(array, 0, size - 1, size);
 }
